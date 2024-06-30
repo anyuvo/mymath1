a24c05bbb4a3b8316997ad0a8a180f07a30cf286
anyuvo <48952341+anyuvo@users.noreply.github.com>
2024-06-30 18:13:03 +0300

    Create mymath.go

---
+++ mymath.go
@@ -0,0 +1,47 @@
+package mymath
+
+import "math"
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+
+func Pow(x float64, n float64) float64 {
+	return math.Pow(x, n)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
